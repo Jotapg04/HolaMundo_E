d432ca424944e582054bb01006c6773729a2ae83
Jessica Perea Guerrero <jpg04@iesemilidarder.com>
2024-02-13 18:49:17 +0100

    Primera versión

--- src/holaMundoE/HolaMundo_Eclipse.java
+++ src/holaMundoE/HolaMundo_Eclipse.java
@@ -3,7 +3,8 @@ package holaMundoE;
 public class HolaMundo_Eclipse {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hola Mundo");
 
 	}
 
